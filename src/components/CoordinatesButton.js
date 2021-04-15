474938a0fabb2d9351cbe2a42dece361f490628e
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-15 09:03:57 -0500

    commit tree 74875353d3c10adbf94338838f16e5fed638f398

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,12 @@
 // Code CoordinatesButton Component Here
+import React from "react";
+class CoordinatesButton extends React.Component {
+  handleClick = (e) => {
+    this.props.onReceiveCoordinates([e.clientY, e.clientX]);
+  };
+  render() {
+    return <button onClick={this.handleClick}>Click Me</button>;
+  }
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,15 @@
 // Code DelayedButton Component Here
+import React from "react";
+class DelayedButton extends React.Component {
+  handleClick = (e) => {
+    e.persist();
+    setTimeout(() => {
+      this.props.onDelayedClick(e);
+    }, this.props.delay);
+  };
+  render() {
+    return <button onClick={this.handleClick}>Delayed click</button>;
+  }
+}
+
+export default DelayedButton;
